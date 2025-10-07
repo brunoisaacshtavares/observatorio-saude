cf88777a2a535cb2c70c5a07ff78d4af1f4a015b
eDusVx <eduardosvx@gmail.com>
2025-10-07 00:12:33 -0300

    feat: migrations

---
+++ observatorio.saude/Migrations/20251006221257_AddFatoLeitoTable.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace observatorio.saude.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFatoLeitoTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "fato_leito",
+                columns: table => new
+                {
+                    cod_cnes = table.Column<long>(type: "bigint", nullable: false),
+                    anomes = table.Column<long>(type: "bigint", nullable: false),
+                    nm_estabelecimento = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    dscr_tipo_unidade = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    qtd_leitos_existentes = table.Column<int>(type: "integer", nullable: false),
+                    qtd_leitos_sus = table.Column<int>(type: "integer", nullable: false),
+                    qtd_uti_total_exist = table.Column<int>(type: "integer", nullable: false),
+                    qtd_uti_total_sus = table.Column<int>(type: "integer", nullable: false),
+                    qtd_uti_adulto_exist = table.Column<int>(type: "integer", nullable: false),
+                    qtd_uti_adulto_sus = table.Column<int>(type: "integer", nullable: false),
+                    qtd_uti_pediatrico_exist = table.Column<int>(type: "integer", nullable: false),
+                    qtd_uti_pediatrico_sus = table.Column<int>(type: "integer", nullable: false),
+                    qtd_uti_neonatal_exist = table.Column<int>(type: "integer", nullable: false),
+                    qtd_uti_neonatal_sus = table.Column<int>(type: "integer", nullable: false),
+                    qtd_uti_queimado_exist = table.Column<int>(type: "integer", nullable: false),
+                    qtd_uti_queimado_sus = table.Column<int>(type: "integer", nullable: false),
+                    qtd_uti_coronariana_exist = table.Column<int>(type: "integer", nullable: false),
+                    qtd_uti_coronariana_sus = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_fato_leito", x => new { x.cod_cnes, x.anomes });
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "fato_leito");
+        }
+    }
+}
---
+++ observatorio.saude/Migrations/20251007023212_AddIndexToFatoLeitoAnomes.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace observatorio.saude.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIndexToFatoLeitoAnomes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_fato_leito_anomes",
+                table: "fato_leito",
+                column: "anomes");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_fato_leito_anomes",
+                table: "fato_leito");
+        }
+    }
+}
